23b00f7d8c414e3d466c065529a89af2a41ea6a5
yugo uchiida <y.uchiida@nirneege.com>
2022-11-20 17:32:27 +0900

    3-5. 投稿データのモデルを定義

--- boardApp/board/admin.py
+++ boardApp/board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# 投稿データ用のモデルを読み込み
+from .models import Post
+
 # Register your models here.
+
+admin.site.register(Post)
--- boardApp/board/models.py
+++ boardApp/board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+# gettext_lazy() で、各言語に対応した文字列に変換して表示できる
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
+
+class Post(models.Model):
+    # スニペットで _() を使ってたので、意味もなく多言語対応...
+    title = models.CharField(_("タイトル"), max_length=50)
+    message = models.TextField(_("メッセージ"))
+    user = models.CharField(_("投稿者"), max_length=50)
+    attached_image = models.ImageField(_("添付画像"), upload_to='attached_images', height_field=None, width_field=None, max_length=None)
+    like = models.IntegerField(_("いいね"))
+    read = models.IntegerField(_("既読者数"))
+    read_text = models.IntegerField(_("既読ユーザー"))
+    
