8de4dcf9f282d653f1c95a8ed9851c571d8fcc23
yugo uchiida <y.uchiida@nirneege.com>
2022-11-20 12:24:06 +0900

    2-10. Todoモデルに項目を追加 - 優先度 - 締切日

--- todoApp/todo/models.py
+++ todoApp/todo/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+# 優先度設定用のタプルを作成
+PRIORITIES = ('high', 'high'), ('normal', 'normal'), ('low', 'low')
+
 # todo のモデルを作成する
 class TodoModel(models.Model):
     # カラム名と型を定義
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    
+    priority = models.CharField(
+        max_length=50,
+        # priority カラムに入るべき値をタプルで指定
+        choices=PRIORITIES
+    )
+    
+    # 締め切り日を設定するカラム
+    duedate = models.DateField()
 
     def __str__(self):
         return self.title
