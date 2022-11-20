96e20c9894a2536e17bed192c8b87740492c437b
yugo uchiida <y.uchiida@nirneege.com>
2022-11-20 10:37:23 +0900

    2-6. ListViewクラスを用いてTodoモデルの一覧を表示する仮実装

--- todoApp/todo/urls.py
+++ todoApp/todo/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import TodoList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('list/', TodoList.as_view())
 ]
--- todoApp/todo/views.py
+++ todoApp/todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+# Todoモデルを読み込み
+from .models import TodoModel
+
+# ListViewをよみこみ
+from django.views.generic.list import ListView
+
 # Create your views here.
+
+class TodoList(ListView):
+    # リスト表示するモデルを指定
+    model = TodoModel
+    
+    # 表示に利用するhtml を指定
+    template_name: str = 'list.html'
