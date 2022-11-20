4460d20a8d8e09f7cae1733c136c3f9f659afe8c
yugo uchiida <y.uchiida@nirneege.com>
2022-11-20 15:11:48 +0900

    3-2. ユーザー登録フォームの表示を仮実装

--- boardApp/board/urls.py
+++ boardApp/board/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import register_user
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/register', register_user),
 ]
--- boardApp/board/views.py
+++ boardApp/board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# ユーザー登録画面と登録の処理
+def register_user(req):
+    # render() で画面描画のレスポンスを返す
+    # 1. リクエストオブジェクト, 2. テンプレートのhtml, 3. 描画に用いるデータ
+    return render(req, 'register_user.html', {})
