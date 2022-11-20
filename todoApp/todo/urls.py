170da299f5a54945e1331b0994c888192f7910ab
yugo uchiida <y.uchiida@nirneege.com>
2022-11-20 13:06:01 +0900

    2-12. Todo追加のViewと画面を作成

--- todoApp/todo/urls.py
+++ todoApp/todo/urls.py
@@ -3,12 +3,16 @@ from django.urls import path, include
 
 from .views import TodoList
 from .views import TodoDetail
+from .views import TodoCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('list/', TodoList.as_view()),
+    path('list/', TodoList.as_view(), name='list'),
     
     # int型でpkを指定して、個別のデータをとる
-    path('detail/<int:pk>', TodoDetail.as_view())
+    path('detail/<int:pk>', TodoDetail.as_view()),
+    
+    # Todo新規作成用のviewを追加
+    path('create/', TodoCreate.as_view(), name='create'),
 ]
--- todoApp/todo/views.py
+++ todoApp/todo/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+# urlのルーティングを遅延解決する関数 reverse_lazyを読み込み
+from django.urls import reverse_lazy
+
 # Todoモデルを読み込み
 from .models import TodoModel
 
 # ListViewをよみこみ
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -22,4 +26,16 @@ class TodoDetail(DetailView):
 
     # 表示に利用するhtml を指定
     template_name: str = 'detail.html'
+
+class TodoCreate(CreateView):
+    # リスト表示するモデルを指定
+    model = TodoModel
+
+    # 表示に利用するhtml を指定
+    template_name: str = 'create.html'
     
+    # 画面上で操作させるフィールドをタプルで指定する
+    fields = ('title', 'memo', 'priority', 'duedate')
+    
+    # データ作成後に遷移するURLを、urls.py のルーティングで指定されたname 属性から取得する
+    success_url = reverse_lazy('list')
